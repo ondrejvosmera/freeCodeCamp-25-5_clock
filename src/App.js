e2e95d40602e15aabd5c357151cfbce9581726d7
ondrejvosmera <ondravosmera@seznam.cz>
2022-11-28 18:44:51 +0100

    regio_sess

--- src/App.js
+++ src/App.js
@@ -7,12 +7,62 @@ import { BiPlayCircle } from 'react-icons/bi'
 import { VscDebugRestart } from 'react-icons/vsc'
 
 class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.loop = undefined;
+  }
+
+  state = {
+    breakCount: 5,
+    sessionCount: 25,
+    clock: 25 * 60,
+    currentTimer: 'Session',
+    loop: undefined,
+    isPlaying: false
+  }
+
+  handlePlayPause = () => {
+    const { isPlaying } = this.state;
+
+    if(isPlaying) {
+      clearInterval(this.loop);
+
+      this.setState ({
+        isPlaying: false
+      });
+    } else {
+      this.setState({
+        isPlaying: true
+      });
+
+      this.loop = setInterval(() => {}, 1000);
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.loop);
+  }
+
+  time_convert(num) { 
+    const minutes = Math.floor(num / 60);  
+    let seconds = num % 60;
+
+    seconds = seconds < 10 ? ('0' + seconds) : seconds;
+    return minutes + ":" + seconds;         
+  }
 
   render () {
 
+    const { 
+      breakCount, 
+      sessionCount, 
+      clock, 
+      currentTimer
+     } = this.state
+
     return (
       <div className='container'>
-        <h1 className='title'>25 + 5 clock</h1>
+        <h1 className='title'>Pomodoro clock</h1>
 
         <div className='break_session'>
 
@@ -20,7 +70,7 @@ class App extends React.Component {
             <div className='break-title'>Break length</div>
             <div className='break-control'>
               <button className='arrow'><FiArrowUpCircle /></button>
-              <div className='break-number'>5</div>
+              <div className='break-number'>{breakCount}</div>
               <button className='arrow'><FiArrowDownCircle /></button>
             </div>
           </div>
@@ -29,15 +79,15 @@ class App extends React.Component {
             <div className='session-title'>Session length</div>
             <div className='session-control'>
               <button className='arrow'><FiArrowUpCircle /></button>
-              <div className='session-number'>5</div>
+              <div className='session-number'>{sessionCount}</div>
               <button className='arrow'><FiArrowDownCircle /></button>
             </div>
           </div>
         </div>
 
           <div className='clock'>
-            <div className='clock-title'>Session</div>
-            <div className='timer'>25:00</div>
+            <div className='clock-title'>{currentTimer}</div>
+            <div className='timer'>{this.time_convert(clock)}</div>
 
             <div className='control'>
               <button className='btn-control'><BiPlayCircle /></button>

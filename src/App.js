e3d6a6abd0dd73111846f2dff154e34e1217d607
ondrejvosmera <ondravosmera@seznam.cz>
2022-11-28 16:39:48 +0100

    html+css_setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
 import React from 'react'
+import './App.css'
+import { FiArrowUpCircle } from 'react-icons/fi'
+import { FiArrowDownCircle } from 'react-icons/fi'
+import { BiPauseCircle } from 'react-icons/bi'
+import { BiPlayCircle } from 'react-icons/bi'
+import { VscDebugRestart } from 'react-icons/vsc'
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
+class App extends React.Component {
+
+  render () {
+
+    return (
+      <div className='container'>
+        <h1 className='title'>25 + 5 clock</h1>
+
+        <div className='break_session'>
+
+          <div className='break'>
+            <div className='break-title'>Break length</div>
+            <div className='break-control'>
+              <button className='arrow'><FiArrowUpCircle /></button>
+              <div className='break-number'>5</div>
+              <button className='arrow'><FiArrowDownCircle /></button>
+            </div>
+          </div>
+        
+          <div className='session'>
+            <div className='session-title'>Session length</div>
+            <div className='session-control'>
+              <button className='arrow'><FiArrowUpCircle /></button>
+              <div className='session-number'>5</div>
+              <button className='arrow'><FiArrowDownCircle /></button>
+            </div>
+          </div>
+        </div>
+
+          <div className='clock'>
+            <div className='clock-title'>Session</div>
+            <div className='timer'>25:00</div>
+
+            <div className='control'>
+              <button className='btn-control'><BiPlayCircle /></button>
+              <button className='btn-control'><BiPauseCircle /></button>
+              <button className='btn-control'><VscDebugRestart /></button>
+            </div>
+          </div>
+          
+      </div>
+    )
+  }
 }
 
 export default App
